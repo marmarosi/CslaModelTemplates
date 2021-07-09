6aff5bc6ce4e0ebb408a7398a2004d36eaa5e01f
Mármarosi József <marmarosi@logikum.hu>
2021-07-09 23:08:03 +0200

    Fix DAL types.

--- CslaModelTemplates.Dal/DAL.cs
+++ CslaModelTemplates.Dal/DAL.cs
@@ -10,6 +10,5 @@ namespace CslaModelTemplates.Dal
         public const string PostgreSQL = "PostgreSql";
         public const string SQLite = "SQLite";
         public const string SQLServer = "SQLServer";
-        public const string Csaba = "Csaba";
     }
 }
